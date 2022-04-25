d2752ae43d1e431c60186c116e59cdf3533e44f0
Amel <amelntamark@gmail.com>
2022-04-24 18:01:20 -0700

    create book class and hardcode books data

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,26 @@ from flask import Blueprint, jsonify
 
 books_bp = ("books_bp", __name__, "/books")
 
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description 
+
+books = [
+    Book(1, "Jane Eyre", "Classic"),
+    Book(2, "Dune", "Science Fiction"),
+    Book(3, "My Year of Rest and Relaxation", "Fiction"),
+    Book(4, "Manufacturing Consent", "Nonfiction")
+]
+
 @books.route("", methods=["GET"])
 def get_books():
     books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
     return jsonify(books_response)
